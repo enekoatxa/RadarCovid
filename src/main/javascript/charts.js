04dcc725aa72d1b50286bb081d42eea7734edf88
--global <eneko.atxa@opendeusto.es>
2020-11-11 11:18:10 +0100

    graph draw functionality

---
+++ src/main/javascript/charts.js
@@ -0,0 +1,149 @@
+var myChart;
+var ageData = Array.from({length: 100}, () => Math.floor(Math.random() * 1000));
+var genderData = [15, 2, 16];
+var occuData = [15, 2, 16, 75, 100];
+var tempoData = [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3];
+
+function paintChart(number){
+	Chart.defaults.global.elements.rectangle.backgroundColor = 'rgba(54, 162, 235, 0.2)';
+	Chart.defaults.global.elements.rectangle.borderColor = 'rgba(54, 162, 235, 1)';
+	Chart.defaults.global.elements.line.backgroundColor = 'rgba(54, 162, 235, 0.2)';
+	Chart.defaults.global.elements.line.borderColor = 'rgba(54, 162, 235, 1)';
+	try{
+		myChart.destroy();
+	} catch (error){}
+	var ctx = document.getElementById('myChart').getContext('2d');
+    updateStatButtons(1);
+	switch (number){
+		case 1:
+		myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: Array.from(Array(100).keys()),
+	        datasets: [{
+	            label: '# of Positives',
+	            data: ageData,
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }]
+	        }
+	    }
+	});
+			break;
+		case 2:
+		updateStatButtons(2);
+		myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: ['Male', 'Female', 'Other'],
+	        datasets: [{
+	            label: '# of Positives',
+	            data: genderData,
+	            backgroundColor: [
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(255, 206, 86, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 99, 132, 1)',
+	                'rgba(255, 206, 86, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }]
+	        }
+	    }
+	});
+		break;
+		case 3:
+		updateStatButtons(3);
+		myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: ['1st sector', '2nd sector', '3rd sector', 'unoccupied', 'student'],
+	        datasets: [{
+	            label: '# of Positives',
+	            data: occuData,
+	            backgroundColor: [
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 99, 132, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }]
+	        }
+	    }
+	});
+		break;
+		case 4:
+		updateStatButtons(4);
+		myChart = new Chart(ctx, {
+	    type: 'line',
+	    data: {
+	        labels: ['2020-1', '2020-2', '2020-3', '2020-4', '2020-5', '2020-6',
+	        	'2020-7', '2020-8', '2020-9', '2020-10', '2020-11', '2020-12',
+	        	'2021-1', '2021-2', '2021-3', '2021-4', '2021-5', '2021-6'],
+	        datasets: [{
+	            label: '# of Positives',
+	            data: tempoData,
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }]
+	        }
+	    }
+	});
+		break;
+	}	
+}
+
+function updateStatButtons(activeButton){
+	for (var i = 1; i <=4 ; i++) {
+		if(activeButton==i){
+			document.getElementById("statsButton"+i).classList.remove("btn-primary");
+   			document.getElementById("statsButton"+i).classList.add("btn-success");
+		}
+		else{
+			document.getElementById("statsButton"+i).classList.add("btn-primary");
+   			document.getElementById("statsButton"+i).classList.remove("btn-success");
+		}
+	}
+}
